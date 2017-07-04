840b2ec11c0eec73595d555d70a46a49da3a0ee1
Pietro Giuffrida <p.giuffrida@crif.com>
2017-07-04 19:15:03 +0200

    Ottimizzazione API

--- check.py
+++ check.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# global limit 580 calls in 3600
+
+import os
+from time import sleep
 from private import *
 import xmlrpc.client
 from datetime import datetime, timedelta
@@ -8,32 +12,104 @@ from datetime import datetime, timedelta
 import logging
 logging.basicConfig(
   format='%(asctime)s\t%(levelname)s\t%(message)s',
-  filename='default.log',
-  level=logging.INFO
-  #level=logging.DEBUG
+  #filename='default.log',
+  #level=logging.INFO
+  level=logging.DEBUG
   )
-logging.getLogger("requests").setLevel(logging.WARNING)
-logging.getLogger("urllib3").setLevel(logging.WARNING)
-
 
 logging.info('*'*20 + ' NUOVA ESECUZIONE ' + '*'*20)
 
+now = datetime.now()
 
+if os.path.exists('disponibilita.json'):
+  disponibilita = json.load(open('disponibilita.json'))
+  disponibilita = disponibilita['next']
+else:
+  sleep(2)
+  disponibilita = datetime.now()
+
+
+if disponibilita < now:
+  delay = disponibilita - now
+  logging.error('Eccessiva frequenza, aspetta {}'.format(delay.seconds))
+
+
+calls = 1
 server = xmlrpc.client.Server(url)
-returnCode, token= server.acquire_token(user, password, pkey)
+returnCode, token = server.acquire_token(user, password, pkey)
 
 props = server.fetch_properties(token)
 lcode = props[0]['lcode']
 
-now = datetime.now()
-prossimi3 = now + timedelta(days=7-now.isoweekday()+14)
-delta = prossimi3 - now
+
+past = now + timedelta(days=7-now.isoweekday()-14)
+future = now + timedelta(days=7-now.isoweekday()+14)
+
+resvs = server.fetch_reservations(token, lcode, past.strftime('%d/%m/%Y'), future.strftime('%d/%m/%Y'))
+calls += 1
+sleep(5)
+
+for i in resvs:
+  i['dfrom'] = datetime.fromtimestamp(i['dfrom'])
+  i['dto'] = datetime.fromtimestamp(i['dto'])
+
 
 data = {}
+delta = future - past
+for d in range(delta.days + 1):
+
+  cur = past + timedelta(days=d)
+  data[cur.strftime('%d/%m/%Y')] = {'resvs': 0}
+
+  logging.debug(cur)
+
+  for i in resvs:
+    if i['dfrom'] >= cur <= i['dto']:
+      data[cur.strftime('%d/%m/%Y')]['resvs'] += 1
+      logging.debug(i['rcode'])
+      print(cur, i['rcode'])
+
+
+
 for i in range(delta.days + 1):
-  cur = now + timedelta(days=i)
-  resvs = server.fetch_reservations_day(token, lcode, cur.strftime('%d/%m/%Y'))
-  data[cur] = resvs
+
+  cur = yesterday + timedelta(days=i)
+  scur = cur.strftime('%d/%m/%Y')
+  data[scur] = {}
+
+  logging.debug('Arrivato a {}'.format(scur))
+
+  resvs = server.fetch_reservations_day(token, lcode, scur)
+  calls += 1
+
+  if len(resvs) == 2 and resvs[0] in [-1, -21]:
+    logging.warning('token scaduto {}'.format(calls))
+    break
+
+  data[scur]['resvs'] = resvs
+
+  if cur <= now:
+
+    logging.debug('Cheking revenues {}'.format(scur))
+
+    rev = 0
+    for res in resvs:
+      invoices = server.fetch_invoices(token, lcode, res['rcode'])
+
+      if len(invoices) == 2 and invoices[0] in [-1, -21]:
+        logging.warning('token scaduto {}'.format(calls))
+        break
+
+      for invoice in invoices:
+        logging.debug(invoice)
+        rev += invoice['concepts']['tt_invoice']
+
+      sleep(2)
+
+    data[scur]['invoices'] = rev
+
+  sleep(3)
+
 
 
 # Chiudere
